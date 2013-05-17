357b82c50eac3665a412d23ed59822a92d4f8292
Andy Lindeman <alindeman@gmail.com>
2013-05-16 21:35:47 -0400

    I think this one is tail recursive

--- problem20.erl
+++ problem20.erl
@@ -4,12 +4,18 @@
 run() ->
   sum(fact(100)).
 
-sum(N) when N =< 0 ->
-  0;
 sum(N) ->
-  (N rem 10) + sum(N div 10).
+  sum(N, []).
 
-fact(N) when N =< 1 ->
-  1;
+sum(N, L) when N =< 0 ->
+  lists:foldl(fun(X, Sum) -> X + Sum end, 0, L);
+sum(N, L) ->
+  sum(N div 10, [(N rem 10)|L]).
+
 fact(N) ->
-  N * fact(N - 1).
+  fact(N, []).
+
+fact(N, L) when N =< 1 ->
+  lists:foldl(fun(X, Prod) -> X * Prod end, 1, L);
+fact(N, L) ->
+  fact(N - 1, [N|L]).
